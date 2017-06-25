afe78d0005d2f68a1a768a204eab978073966d68
Jesse Palmer <jesselpalmer@gmail.com>
2017-06-24 18:18:50 -0700

    refactor: clean up directory structure

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/', express.static('./src'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
