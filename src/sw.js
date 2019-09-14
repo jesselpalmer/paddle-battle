b075860c7bcbfc1af1c4e881a2ed64c43189fca6
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-13 23:02:55 -0700

    feat: now pwa

---
+++ src/sw.js
@@ -0,0 +1,37 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/css/style-1.0.css',
+        '/js/libs/jquery-1.7.1.min.js',
+        '/js/libs/collision-1.0.4.js',
+        '/resources/blup.mp3',
+        '/resources/youWin.mp3',
+        '/resources/youLose.mp3'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+    }
+  }));
+});
